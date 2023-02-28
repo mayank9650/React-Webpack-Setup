2fa32c6cb165646b05c1b5e572d8b17194a1f7e7
Mayank Bhandari <mayankbhandari9650@gmail.com>
2023-02-28 12:07:20 +0530

    Basic setup - Images integration code

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./style.css"
+import reactIcon from './React.png'
+import ReactLogo from './logo.svg'
+
 export const App = () =>{
-    return <h2>React Code Setup</h2>
+    return <>
+    <h2>React Code Setup</h2>
+    <img src={reactIcon} alt="React Icon" width={'100px'}/>
+    <img src={ReactLogo} alt="React Logo" width={'100px'}/>
+
+    </>
+    
 }
