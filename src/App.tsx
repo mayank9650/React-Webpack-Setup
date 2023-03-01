e450f89338a349fcb2dfbe1730bc9e90f9d22312
Mayank Bhandari <mayankbhandari9650@gmail.com>
2023-03-01 15:34:37 +0530

    Basic setup - Webpack config for prod and dev

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ReactLogo from './logo.svg'
 
 export const App = () =>{
     return <>
-    <h2>React Code Setup</h2>
+    <h2>React Code Setup {process.env.NODE_ENV} {process.env.name}</h2>
     <img src={reactIcon} alt="React Icon" width={'100px'}/>
     <img src={ReactLogo} alt="React Logo" width={'100px'}/>
 
