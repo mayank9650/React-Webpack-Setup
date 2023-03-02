e9d85f4aae3262e86e676ee65b539971ce28a5f5
Mayank Bhandari <mayankbhandari9650@gmail.com>
2023-03-02 10:20:17 +0530

    Basic setup - prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import "./style.css"
+import './style.css'
 import reactIcon from './React.png'
 import ReactLogo from './logo.svg'
 
-export const App = () =>{
-    return <>
-    <h2>React Code Setup {process.env.NODE_ENV} {process.env.name}</h2>
-    <img src={reactIcon} alt="React Icon" width={'100px'}/>
-    <img src={ReactLogo} alt="React Logo" width={'100px'}/>
+export const App = () => {
+  return (
+    <>
+      <h2>
+        React Code Setup {process.env.NODE_ENV} {process.env.name}
+      </h2>
 
+      <img src={reactIcon} alt="React Icon" width={'100px'} />
+      <img src={ReactLogo} alt="React Logo" width={'100px'} />
     </>
-    
+  )
 }
